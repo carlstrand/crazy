b4d252fb87da0bb570a435247f2541498bfe2aa8
Carlstrand <mattias@zvea.co>
2019-02-12 23:42:15 +0100

    gh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible nano git rsync -y
+sudo apt-get upgrade -y
+
+
+git clone https://github.com/carlstrand/crazy.git
