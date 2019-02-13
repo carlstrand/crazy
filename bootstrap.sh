1718f68e0c226a78e44859341cb0e037e4ff5fd1
Carlstrand <mattias@zvea.co>
2019-02-13 01:37:39 +0100

    dsf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+sudo apt update -y
 sudo apt install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update -y
-sudo apt-get install -y ansible nano git rsync
+sudo apt update -y
+sudo apt install -y ansible nano git rsync apt-utils locales
+git clone https://github.com/carlstrand/crazy.git
+cd ~/crazy
+sudo ansible-playbook -i hosts.yml install/playbooks/main.yml -vvv
