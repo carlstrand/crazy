ee0c0329c423f0bf21aca9ffee285b377d2347a3
Carlstrand <mattias@zvea.co>
2019-02-13 10:20:38 +0100

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ sudo apt update -y
 sudo apt install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt update -y
-sudo apt install -y ansible nano git rsync apt-utils locales
+sudo apt install -y ansible git rsync apt-utils locales
 git clone https://github.com/carlstrand/crazy.git
 cd ~/crazy
 sudo ansible-playbook -i hosts.yml install/playbooks/main.yml -vvv
--- play-sync.sh
+++ play-sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+sudo bash sync.sh
 sudo ansible-playbook -i hosts.yml install/playbooks/main.yml -vvv
